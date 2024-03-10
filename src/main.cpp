68d407dfa685d8236137bf46fb64e79745296c43
Pavel Kalugin <pavel@pavelthebest.me>
2024-03-10 10:35:28 +0300

    simple Qt project

---
+++ src/main.cpp
@@ -0,0 +1,11 @@
+// module or class includes
+#include <QtCore>
+
+// text stream is text-codec aware
+QTextStream cout(stdout, QIODevice::WriteOnly);
+
+int main()
+{
+    cout << "Hello, world!" << Qt::endl;
+}
+
